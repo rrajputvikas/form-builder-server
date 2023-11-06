aea703303ceda43a6d0c33be4ba045b35b71fb02
Vikas Rajput <vikassinghrajput551@gmail.com>
2023-11-06 22:35:24 +0530

    updated code

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const {GridFsStorage} = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const Form = require("./db/Form");
@@ -30,6 +32,32 @@ app.use(
   })
 );
 
+// Upload Header
+app.post("/create-form", async (req, res) => {
+  const { formName, formDescription } = req.body;
+  try {
+    if (!formName || !formDescription) {
+      return res
+      .status(400)
+      .json({ error: "FormName and formDescription are required." });
+    }
+    
+    const newForm = new Form({
+      name: formName,
+      description: formDescription,
+      headerImage: null,
+      questions: null
+    });
+    const savedForm = await newForm.save();
+
+    res.status(201).json(savedForm);
+  } catch (error) {
+    console.error("Error saving data to the database:", error);
+    res.status(500).json({ error: "Error saving data to the database" });
+  }
+});
+
+// Upload Header Image
 const upload = multer({ dest: 'uploads/' });
 
 app.post("/update/:id", upload.single('image'), async (req, res) => {
@@ -55,29 +83,28 @@ app.post("/update/:id", upload.single('image'), async (req, res) => {
   }
 });
 
-app.post("/create-form", async (req, res) => {
-  const { formName, formDescription } = req.body;
-  try {
-    if (!formName || !formDescription) {
-      return res
-      .status(400)
-      .json({ error: "FormName and formDescription are required." });
-    }
-    
-    const newForm = new Form({
-      name: formName,
-      description: formDescription,
-      headerImage: null
-    });
-    const savedForm = await newForm.save();
+// Upload Form
+  app.post("/save-form/:id", async (req, res) => {
+  
+    try {
+      const id = req.params.id;
 
-    res.status(201).json(savedForm);
-  } catch (error) {
-    console.error("Error saving data to the database:", error);
-    res.status(500).json({ error: "Error saving data to the database" });
+      const form = await Form.findById(id);
+      if (!form) {
+        return res.status(404).json({ error: 'Document not found' });
+      }
+      form.questions = req.body;
+      console.log(form)
+      await form.save();
+      
+      res.json({ message: 'Data Saved successfully' });
+    } catch(error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "An error occurred" });
   }
-});
+})
 
+// Receive Header
 app.get("/forms/:id", async (req, res) => {
   try {
     const id = req.params.id;
@@ -96,4 +123,31 @@ app.get("/forms/:id", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+//Receive Forms
+app.get("/forms", async (req, res) => {
+  try {
+    const data  = await Form.find();
+
+    if (!data) {
+      return res.status(404).json({ error: 'Data not found' });
+    }
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Receive Image of forms
+app.get("/image/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const imagePath = path.join(__dirname, 'uploads', filename);
+
+  if (fs.existsSync(imagePath)) {
+    res.sendFile(imagePath);
+  } else {
+    res.status(404).json({ error: 'Image not found' });
+  }
 });
